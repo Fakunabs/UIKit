8289f23f46d75ed20d33c4faf44044dbaadfec57
Fakunabs <gempro0501@gmail.com>
2023-04-10 17:12:37 +0700

    Login App

--- Hello/Hello/ViewController.swift
+++ Hello/Hello/ViewController.swift
@@ -45,7 +45,6 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewdidappear")
