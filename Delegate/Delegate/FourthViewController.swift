481a6129631505d5363415045ebbfffcfb1bf395
Fakunabs <gempro0501@gmail.com>
2023-04-14 09:26:43 +0700

    Handle-delegate-table-view

--- Delegate/Delegate/FourthViewController.swift
+++ Delegate/Delegate/FourthViewController.swift
@@ -8,33 +8,55 @@
 import UIKit
 
 class FourthViewController: UIViewController {
+    
     @IBOutlet weak var myTableView: UITableView!
     
-    var image:[String] = ["Test"]
-    var imageTest:[String] = ["test"]
+    struct Meme {
+        let imageName: String
+        var title: String
+    }
     
+    var table: [Meme] = [
+        Meme(imageName: "test", title: "Test"),
+        Meme(imageName: "cat", title: "Cat"),
+        Meme(imageName: "dog", title: "Dog"),
+        Meme(imageName: "fire", title: "Fire"),
+        Meme(imageName: "look", title: "Look"),
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Fourth View Controller"
-        
         myTableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
         myTableView.dataSource = self
         myTableView.delegate = self
-        
     }
-    
-    
 }
 // MARK: - Table view
-extension FourthViewController:UITableViewDataSource, UITableViewDelegate {
+extension FourthViewController: UITableViewDataSource, UITableViewDelegate, dataSendToFourthView
+{    
+    func data(text: String, indexPath: IndexPath) {
+        table[indexPath.row].title = text
+        myTableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return image.count
+        return table.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-        cell.myImage.image = UIImage(named: imageTest[indexPath.row])
-        cell.myLabel.text = image[indexPath.row]
+        cell.myImage.image = UIImage(named: table[indexPath.row].imageName)
+        cell.myLabel.text = table[indexPath.row].title
         return cell
     }
-}
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {        
+        let vc = FifthViewController()
+        vc.delegate = self
+        vc.indexPath = indexPath
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    
+    }
+}   
