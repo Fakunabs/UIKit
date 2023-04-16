47c191d8f0b41d69183bee46ef32d31bb46179e7
Fakunabs <gempro0501@gmail.com>
2023-04-16 14:58:02 +0700

    Handle-collection-view

---
+++ Delegate/Delegate/CollectionViewCell 2.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewCell.swift
+//  Delegate
+//
+//  Created by Fakunabs on 14/04/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var myLandScapeImage: UIImageView!
+    
+    @IBOutlet weak var myLandScapeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
